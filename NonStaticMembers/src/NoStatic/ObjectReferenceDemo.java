16238450ba0c257a5a82ed31643eef7b30d92d3e
AMS <alonso_mendess@mail.com>
2021-10-21 12:02:23 -0300

    Atualização

---
+++ NonStaticMembers/src/NoStatic/ObjectReferenceDemo.java
@@ -0,0 +1,18 @@
+package NoStatic;
+
+public class ObjectReferenceDemo {
+	
+	int x;
+	
+	ObjectReferenceDemo() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		 ObjectReferenceDemo ord = new ObjectReferenceDemo();
+		 
+		 System.out.println(ord.x);
+	}
+
+}
