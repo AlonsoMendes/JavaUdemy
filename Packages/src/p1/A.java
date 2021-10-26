e4d573f2237ecae03a69cf0dce592d73037fe824
AMS <alonso_mendess@mail.com>
2021-10-26 13:50:02 -0300

    Criando os Packages

---
+++ Packages/src/p1/A.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class A {
+	
+	public static void a1() {
+		System.out.println("Insede a1");
+	}
+	
+	public void a2() {
+		System.out.println("Insede a2");
+	}
+}
---
+++ Packages/src/p2/C.java
@@ -0,0 +1,21 @@
+package p2;
+
+import p1.A;
+import static  p1.A.*;
+import p1.p3.E;
+
+public class C {
+	
+	public static void main(String[] args) {
+		
+		A.a1();
+		A objectA = new A();
+		
+		objectA.a2();
+	
+		E obkectE= new E();
+		obkectE.e1();
+		
+	}
+
+}
