c775cb8201172ab1e40c1c7946d41b3ece048a89
AMS <alonso_mendess@mail.com>
2021-10-26 11:12:48 -0300

    Criação do MorePrograms

---
+++ MorePrograms/src/flowcontrol/For.java
@@ -0,0 +1,14 @@
+package flowcontrol;
+
+public class For {
+	public static void main(String[] args) {
+		
+		int i= 1;
+		
+		for ( System.out.println("XYZ"); i < 10; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
---
+++ MorePrograms/src/flowcontrol/While.java
@@ -0,0 +1,14 @@
+package flowcontrol;
+
+public class While {
+	
+	public static void main(String[] args) {
+		
+		while (true) {
+			System.out.println("Hello");
+		break;
+		}
+		
+	}
+
+}
